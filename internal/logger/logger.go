76e7f9d975226579809be0dd44d04398aff2f22f
takeshi.koenuma <t.koenuma2@gmail.com>
2022-05-10 00:56:53 +0900

    refactor logger interface

--- internal/logger/logger.go
+++ internal/logger/logger.go
@@ -2,23 +2,29 @@ package logger
 
 import "fmt"
 
-type Logger struct {
+//go:generate moq -out logger_moq.go . Logger
+
+type Logger interface {
+	Debug(format string, a ...any)
+	Info(format string, a ...any)
+}
+type DefaultLogger struct {
 	isDebug bool
 }
 
-func NewLogger(mode string) (l Logger) {
+func NewLogger(mode string) (l DefaultLogger) {
 	if mode == "1" {
 		l.isDebug = true
 	}
 	return l
 }
 
-func (l Logger) Debug(format string, a ...any) {
+func (l DefaultLogger) Debug(format string, a ...any) {
 	if l.isDebug {
 		fmt.Printf(format, a...)
 	}
 }
 
-func (l Logger) Info(format string, a ...any) {
+func (l DefaultLogger) Info(format string, a ...any) {
 	fmt.Printf(format, a...)
 }
