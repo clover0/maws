db2d539fda08986514b7c6df9c452662a31ca31f
takeshi.koenuma <t.koenuma2@gmail.com>
2022-05-09 23:40:27 +0900

    add profile test

---
+++ internal/aws/profile_test.go
@@ -0,0 +1,70 @@
+package aws_test
+
+import (
+	"maws/internal/aws"
+	"os"
+	"testing"
+)
+
+func createTempProfileFile() string {
+	profile := `
+[default]
+aws_key=xxx
+
+[profile account.Admin]
+key=xxx
+
+[profile account.admin]
+key=xxx
+
+[profile account.Admin.45]
+key=xxx
+
+`
+	file, err := os.CreateTemp("", "test")
+	if err != nil {
+		panic(err)
+	}
+	defer file.Close()
+	file.WriteString(profile)
+	return file.Name()
+}
+
+func TestFindProfiles(t *testing.T) {
+
+	tests := []struct {
+		Name           string
+		FileName       string
+		Filter         string
+		ExpectProfiles int
+	}{
+		{
+			Name:           "match partial",
+			FileName:       createTempProfileFile(),
+			Filter:         "Admin",
+			ExpectProfiles: 2,
+		},
+		{
+			Name:           "match prefix",
+			FileName:       createTempProfileFile(),
+			Filter:         "account",
+			ExpectProfiles: 3,
+		},
+		{
+			Name:           "not match",
+			FileName:       createTempProfileFile(),
+			Filter:         "not-exist",
+			ExpectProfiles: 0,
+		},
+	}
+
+	for _, c := range tests {
+		t.Run(c.Name, func(t *testing.T) {
+			target := aws.FindProfiles(c.FileName, c.Filter)
+			if len(target) != c.ExpectProfiles {
+				t.Errorf("shoud be %d but actual %d", c.ExpectProfiles, len(target))
+			}
+		})
+	}
+
+}
